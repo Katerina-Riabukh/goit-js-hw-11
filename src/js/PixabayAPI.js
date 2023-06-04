6a92a386ba1b08d3e349f6066b24fecc0e774200
Katrysya7773 <121060967+Katrysya7773@users.noreply.github.com>
2023-06-04 21:44:43 +0200

    css + new files

---
+++ src/js/PixabayAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+
+export class PixabayAPI{
+    #API_KEY = "36945687-a4e7966ed6349b63eadd861cc";
+    #PATH = "https://pixabay.com/api/"
+
+
+
+
+
+
+
+
+
+
+
+    
+}
---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+
+const refs = {
+    form: document.querySelector('.search-form'),
+    gallery: document.querySelector('.gallery'),
+    BtnLoadmore: document.querySelector('.load-more'),
+
+
+}
+
+export default refs;
